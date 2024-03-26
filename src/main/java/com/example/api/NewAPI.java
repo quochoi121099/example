e9bd1088b8f5e7d11c74642598020d48843fceb4
quochoi <quoc.hoi121099@gmail.com>
2024-03-26 16:31:55 +0700

    first commit

---
+++ src/main/java/com/example/api/NewAPI.java
@@ -0,0 +1,29 @@
+package com.example.api;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.dto.NewDTO;
+
+@CrossOrigin
+@RestController
+public class NewAPI {
+
+    @PostMapping(value = "/new")
+    public NewDTO createNew(@RequestBody NewDTO model) {
+        return model;
+    }
+
+    @PutMapping(value = "/new")
+    public NewDTO updateNew(@RequestBody NewDTO model) {
+        return model;
+    }
+
+    @DeleteMapping(value = "/new")
+    public void deleteNew(@RequestBody long[] ids) {
+
+    }
+}
---
+++ src/main/webapp/WEB-INF/decorators/web.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<%@ include file="/common/web/header.jsp"%>
+<sitemesh:write property="body"/>
+<%@ include file="/common/web/footer.jsp"%>
+</body>
+</html>
